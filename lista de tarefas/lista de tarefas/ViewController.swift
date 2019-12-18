3bc364760f05291116261aea49c5ac9223b2d417
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-12-18 15:42:14 -0300

    create ui

---
+++ lista de tarefas/lista de tarefas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lista de tarefas
+//
+//  Created by César  Ferreira on 18/12/19.
+//  Copyright © 2019 César  Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
